d448ebc3b9973f2e6553f7b17093a1d5927cacc8
farrikhah <abdausifa@gmail.com>
2026-06-20 10:45:27 +0700

    modul 9

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,25 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
-
-    // Menentukan nama tabel secara spesifik
-    protected $table = 'mahasiswas';
-
-    // Mendaftarkan kolom yang boleh diisi (Mass Assignment) sesuai kebutuhan Modul 9
     protected $fillable = ['nim', 'nama', 'prodi_id', 'status', 'alamat', 'foto'];
 
-    /**
-     * Relasi Kebalikan (Belongs To)
-     * Menandakan bahwa setiap mahasiswa berafiliasi ke satu Program Studi
-     */
     public function prodi() 
     { 
-        return $this->belongsTo(Prodi::class, 'prodi_id');
+        return $this->belongsTo(Prodi::class);
     }
 }
--- app/Models/Prodi.php
+++ app/Models/Prodi.php
@@ -2,14 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Prodi extends Model
 {
-    use HasFactory;
-    protected $fillable = ['nama_prodi'];
-
     public function mahasiswas()
     {
         return $this->hasMany(mahasiswa::class);
